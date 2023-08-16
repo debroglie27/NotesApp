934bdb18c7bb75df1aceac673deaf202c8b94f40
Arijeet De <arijeetde@gmail.com>
2023-08-16 16:54:30 +0530

    making main.js entry point

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const fetch = require('node-fetch');
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.port || 3000
+app.listen(port, () => {
+    console.log(`Starting Server at ${port}`);
+});
+app.use(express.static('public'));
